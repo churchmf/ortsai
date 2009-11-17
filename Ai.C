4201223e64b076ab36152ae8ee75c1d5fbcee21b
churchmf <churchmf@6f3d44d6-d3b6-11de-9aee-2b7272db14c4>
2009-11-17 20:39:22 +0000

    Main class for AI
    git-svn-id: https://orts3cf.googlecode.com/svn/trunk@3 6f3d44d6-d3b6-11de-9aee-2b7272db14c4

---
+++ Ai.C
@@ -0,0 +1,17 @@
+/*
+ * Ai.C
+ *
+ *  Created on: Nov 17, 2009
+ *      Author: mfchurch
+ */
+
+#include "Ai.H"
+
+Ai::Ai() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Ai::~Ai() {
+	// TODO Auto-generated destructor stub
+}
