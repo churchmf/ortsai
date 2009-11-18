30a03e4b3a9371f9afe06e4f5ef7b1258b91d520
churchmf <churchmf@6f3d44d6-d3b6-11de-9aee-2b7272db14c4>
2009-11-18 22:31:15 +0000

    git-svn-id: https://orts3cf.googlecode.com/svn/trunk@86 6f3d44d6-d3b6-11de-9aee-2b7272db14c4

---
+++ churchfarnsworth/src/Ai.C
@@ -0,0 +1,16 @@
+/*
+ * Ai.C
+ *
+ *  Created on: Nov 17, 2009
+ *      Author: mfchurch
+ */
+#include "Ai.H"
+
+Ai::Ai() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Ai::~Ai() {
+	// TODO Auto-generated destructor stub
+}
---
+++ churchfarnsworth/src/Lieutenant.C
@@ -0,0 +1,17 @@
+/*
+ * Lieutenant.C
+ *
+ *  Created on: Nov 17, 2009
+ *      Author: mfchurch
+ */
+
+#include "Lieutenant.H"
+
+Lieutenant::Lieutenant() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Lieutenant::~Lieutenant() {
+	// TODO Auto-generated destructor stub
+}
