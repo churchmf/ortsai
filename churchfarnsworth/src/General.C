565890726e41d2c2491b222a733faf01875f11e7
churchmf <churchmf@6f3d44d6-d3b6-11de-9aee-2b7272db14c4>
2009-11-19 17:45:09 +0000

    git-svn-id: https://orts3cf.googlecode.com/svn/trunk@109 6f3d44d6-d3b6-11de-9aee-2b7272db14c4

--- churchfarnsworth/src/General.C
+++ churchfarnsworth/src/General.C
@@ -15,3 +15,26 @@ General::General() {
 General::~General() {
 	// TODO Auto-generated destructor stub
 }
+
+
+//General Loop
+/*
+ * for each tile in grid:
+  if tile contains marine:
+    if friendly:
+      tile.risk -= 1
+    else:
+      tile.risk += 1
+  if tile contains tank:
+    if friendly:
+      if deployed:
+        tile.risk -= 5
+      else:
+        tile.risk -= 3
+    else:
+      if deployed:
+        tile.risk += 5
+      else:
+        tile.risk += 3
+depreciate tile value
+ */
