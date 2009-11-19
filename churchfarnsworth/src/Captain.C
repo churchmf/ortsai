72a67e453c62696259b3f8a6a54696d4d1669240
churchmf <churchmf@6f3d44d6-d3b6-11de-9aee-2b7272db14c4>
2009-11-19 02:23:23 +0000

    git-svn-id: https://orts3cf.googlecode.com/svn/trunk@93 6f3d44d6-d3b6-11de-9aee-2b7272db14c4

---
+++ churchfarnsworth/src/Captain.C
@@ -0,0 +1,17 @@
+/*
+ * Captain.C
+ *
+ *  Created on: Nov 17, 2009
+ *      Author: mfchurch
+ */
+
+#include "Captain.H"
+
+Captain::Captain() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Captain::~Captain() {
+	// TODO Auto-generated destructor stub
+}
---
+++ churchfarnsworth/src/General.C
@@ -0,0 +1,17 @@
+/*
+ * General.C
+ *
+ *  Created on: Nov 17, 2009
+ *      Author: mfchurch
+ */
+
+#include "General.H"
+
+General::General() {
+	// TODO Auto-generated constructor stub
+
+}
+
+General::~General() {
+	// TODO Auto-generated destructor stub
+}
