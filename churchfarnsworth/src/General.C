5a4e044df573f0f7295e95e51c60148bdb599ead
churchmf <churchmf@6f3d44d6-d3b6-11de-9aee-2b7272db14c4>
2009-11-20 01:28:46 +0000

    git-svn-id: https://orts3cf.googlecode.com/svn/trunk@112 6f3d44d6-d3b6-11de-9aee-2b7272db14c4

--- churchfarnsworth/src/General.C
+++ churchfarnsworth/src/General.C
@@ -7,13 +7,103 @@
 
 #include "General.H"
 
-General::General() {
-	// TODO Auto-generated constructor stub
+#include "Application.H"
+#include "Game.H"
+#include "GameObj.H"
+#include "GameStateModule.H"
+
+//constant values for marine and tanks used with unit.GetType()
+const sint4 marine = 1;
+const sint4 tank = 2;
+
+General::General(sint4 mapWidth, sint4 mapHeight)
+{
+	width = mapWidth;
+	height = mapHeight;
+
+	CreateGrid();
+}
+
+General::~General()
+{
 
 }
 
-General::~General() {
-	// TODO Auto-generated destructor stub
+void General::CreateGrid()
+{
+	grid = new Tile*[width];
+	for (int i = 0; i < width; ++i)
+	{
+		grid[i] = new Tile[height];
+		for (int j = 0; j < height; ++j)
+		{
+			grid[i][j].x = i;
+			grid[i][j].y = j;
+			grid[i][j].risk = 0;
+		}
+	}
+}
+
+void General::SetEnemies(Vector<Unit> theEnemies)
+{
+	enemies = theEnemies;
+}
+
+void General::Loop()
+{
+	for (int i = 0; i < width; ++i)
+	{
+		for (int j = 0; j < height; ++j)
+		{
+			grid[i][j].risk = 0;
+		}
+	}
+
+	for(size_t i(0); i<enemies.size(); ++i)
+	{
+		Unit & enemy(enemies[i]);
+		sint4 type = enemy.GetType();
+		vec2 location = enemy.GetPosition();
+		sint4 riskValue = 0;
+
+		if (type == marine)
+		{
+			riskValue = 1;
+		}
+		else if (type == tank)
+		{
+			//if tank is sieged
+			if (enemy.GetMode() == 2)
+			{
+				riskValue = 5;
+			}
+			else
+			{
+				riskValue = 3;
+			}
+		}
+		Tile tile = grid[location.x][location.y];
+		if (tile.risk > 0)
+			tile.risk *= 0.9;
+
+		if (tile.risk < 50)
+			tile.risk += riskValue;
+	}
+}
+
+void General::Print()
+{
+	for (int i = 0; i < width; ++i)
+		{
+			for (int j = 0; j < height; ++j)
+			{
+				std::cout << "\t";
+				sint4 risk = grid[i][j].risk;
+				std::cout << "[" << risk << "]" << std::endl;
+			}
+			std::cout << std::endl;
+		}
+	std::cout << std::endl;
 }
 
 
