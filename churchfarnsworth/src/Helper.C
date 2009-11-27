6bf656b76bc9fbeca958af115b6a3d25fc725ece
guidoanchovis <guidoanchovis@6f3d44d6-d3b6-11de-9aee-2b7272db14c4>
2009-11-27 01:00:26 +0000

    git-svn-id: https://orts3cf.googlecode.com/svn/trunk@175 6f3d44d6-d3b6-11de-9aee-2b7272db14c4

--- churchfarnsworth/src/Helper.C
+++ churchfarnsworth/src/Helper.C
@@ -1,15 +1,15 @@
 #include "Helper.H"
+#include "Application.H"
 
 #include "Game.H"
 #include "GameObj.H"
-#include "Movement.H"
 
 // GameObj::sod contains pointers to commonly accessed data fields
 // GameObj is a subclass of ScriptObj
 // All ScriptObjs have data fields with can be accessed with methods like
 // ScriptObj::has_attr() and ScriptObj::get_int()
 
-template<typename T>
+template<typename T> 
 const T & CheckPtr(const T * pointer, const T & defValue)
 {
 	return pointer ? *pointer : defValue;
@@ -22,37 +22,69 @@ int CheckObjInt(const ScriptObj * object, const char * name, int defValue)
 	return ptr ? ptr->to_int() : defValue;
 }
 
-Unit::Unit(const Game & game, ScriptObj * object) :
-	game(&game),
-	unit(object->get_GameObj()),
-	weapon(unit->component("weapon"))
+Unit::Unit(Application & app, ScriptObj * object) : 
+	app(app),
+	unit(*object->get_GameObj()),
+	weapon(unit.component("weapon")) 
 {
+	
+}
 
+bool Unit::IsOwned() const
+{
+	const Game & game(app.GetGame());
+	return *unit.sod.owner == game.get_client_player();
+}
+
+bool Unit::IsEnemy() const
+{
+	const Game & game(app.GetGame());
+	const sint4 owner(*unit.sod.owner);
+	return owner != game.get_client_player() && owner != game.get_player_num();
+}
+
+bool Unit::IsCritter() const
+{
+	const Game & game(app.GetGame());
+	return *unit.sod.owner == game.get_player_num();
 }
 
 bool Unit::IsAlive() const
 {
-	return unit->sod.in_game && !unit->is_dead();
+	return unit.sod.in_game && !unit.is_dead();
 }
 
 bool Unit::IsMoving() const
 {
-	return GetSpeed() != 0;
+	if(task)
+	{
+		Movement::Task::Status status(task->getStatus());
+		return status == Movement::Task::PLANNING || status == Movement::Task::RUNNING;
+	}
+	else
+	{
+		return GetSpeed() != 0;
+	}
 }
 
-sint4 Unit::GetMaxSpeed() const
+bool Unit::CanMove() const
 {
-	return CheckPtr(unit->sod.max_speed,0);
+	return GetMaxSpeed() > 0;
+}
+
+sint4 Unit::GetMaxSpeed() const 
+{ 
+	return CheckPtr(unit.sod.max_speed,0); 
 }
 
 sint4 Unit::GetMaxHitpoints() const
 {
-	return CheckObjInt(unit,"max_hp",0);
+	return CheckObjInt(&unit,"max_hp",0);
 }
 
-bool Unit::HasWeapon() const
-{
-	return weapon != 0;
+bool Unit::HasWeapon() const 
+{ 
+	return weapon != 0; 
 }
 
 sint4 Unit::GetWeaponMinDamage() const
@@ -78,49 +110,63 @@ sint4 Unit::GetWeaponRange() const
 vec2 Unit::GetPosition() const
 {
 	vec2 position;
-	unit->get_center(position.x,position.y);
+	unit.get_center(position.x,position.y);
 	return position;
 }
 
-sint4 Unit::GetMode() const
-{
-	return *unit->get_int_ptr("mode");
-}
-
-sint4 Unit::GetSpeed() const
-{
-	return CheckPtr(unit->sod.speed,0);
+sint4 Unit::GetSpeed() const 
+{ 
+	return CheckPtr(unit.sod.speed,0); 
 }
 
 sint4 Unit::GetHitpoints() const
 {
-	return CheckObjInt(unit,"hp",0);
+	return CheckObjInt(&unit,"hp",0);
 }
 
-sint4 Unit::GetType() const
+real8 Unit::GetDistanceTo(const Unit & other) const
 {
-	const sint4 unittypediv = 3;
-	//returns 1 for marine and 2 for tank
-	return *unit->sod.radius / unittypediv;
-}
-
-bool Unit::InCombat() const
-{
-	uint4 dmg = unit->dir_dmg;
-	return dmg!=0;
+	return unit.distance_to(other.unit);
 }
 
 // Actions are specified by calling set_action on a ScriptObj
 // Actions like movement are called on the GameObj itself (which is a subclass of ScriptObj)
 // Actions like attacking are called on a component of the GameObj, in this case, the weapon
 
-void Unit::MoveTo(const vec2 & location, sint4 speed, Movement::Context* mc)
+void Unit::Stop()
 {
+	// If we have a pathing task, cancel it
+	if(task)
+	{
+		task->cancel();
+		task.reset();
+	}
+
+	// Tell unit to move to current location with speed zero
+	Vector<sint4> args;
+	const vec2 position(GetPosition());
+	args.push_back(position.x);
+	args.push_back(position.y);
+	args.push_back(0);
+	unit.set_action("move", args);
+}
+
+void Unit::MoveTo(const vec2 & location, sint4 speed)
+{
+	Stop();
+
 	Vector<sint4> args;
 	args.push_back(location.x);
 	args.push_back(location.y);
 	args.push_back(speed);
-	unit->set_action("move", args);
+	unit.set_action("move", args);
+}
+
+void Unit::PathTo(Movement::Goal::const_ptr goal)
+{
+	Stop();
+
+	task = app.GetMoveContext().moveUnit(&unit, goal);
 }
 
 void Unit::Attack(const Unit & target)
@@ -128,6 +174,6 @@ void Unit::Attack(const Unit & target)
 	if(!weapon) return;
 
 	Vector<sint4> args;
-	args.push_back(game->get_cplayer_info().get_id(target.unit));
+	args.push_back(app.GetGame().get_cplayer_info().get_id(&target.unit));
 	weapon->set_action("attack", args);
 }
