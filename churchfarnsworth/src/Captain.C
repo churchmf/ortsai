0d119064c4c65895f0fa704258791587a93d7be6
guidoanchovis <guidoanchovis@6f3d44d6-d3b6-11de-9aee-2b7272db14c4>
2009-11-30 01:55:19 +0000

    git-svn-id: https://orts3cf.googlecode.com/svn/trunk@241 6f3d44d6-d3b6-11de-9aee-2b7272db14c4

--- churchfarnsworth/src/Captain.C
+++ churchfarnsworth/src/Captain.C
@@ -121,7 +121,7 @@ void Captain::Loop()
 				//choose safe deployment location towards endangered squad
 				vec2 aidLocation = GetAidRequestLocation();
 				//std::cout << aidLocation.x << "," << aidLocation.y << std::endl;
-				//lieutenant->MoveTo(aidLocation);
+				lieutenant->MoveTo(aidLocation);
 			}
 			else
 			{
